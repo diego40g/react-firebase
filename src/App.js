d7109f7b7bd28d53b9f0aaa2214cd3f0d0ec1bf5
diego40g <diegopaznaula@hotmail.com>
2022-07-01 04:13:54 -0500

    adding bootstrap and route into app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { WebsiteList } from "./components/WebsiteList";
+import { WebsiteForm } from "./components/WebsiteForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<WebsiteList />} />
+          <Route path="add" element={<WebsiteForm />} />
+          <Route path="edit/:id" element={<WebsiteForm />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
